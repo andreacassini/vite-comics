23f5ae1caf6e4a1deb2c8143fd7306a342aad0dd
andreacassini <andrea1316@hotmail.it>
2023-06-21 10:41:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  AppHeader,
+  AppMain,
+  AppFooter
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
