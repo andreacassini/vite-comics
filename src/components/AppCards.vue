1966b639aa62fe8db00fb8c477d306edfeeb4852
andreacassini <andrea1316@hotmail.it>
2023-06-24 19:11:27 +0200

    add component and style card

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,15 +1,23 @@
 <script>
 export default {
-    
+    props:{
+        image: String,
+        title: String
+    }
 }
 </script>
 
 <template>
     <div>
-        
+        <img :src="image" alt="card">
+        <h1>{{ title }}</h1>
     </div>
 </template>
 
 <style lang="scss">
-    
+img{
+    aspect-ratio: 3/4;
+    object-fit: cover;
+    max-width: 100%;
+}
 </style>
